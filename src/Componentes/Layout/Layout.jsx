f7215d45dfaa1c76fb29b6bbf915e2ff423b207f
GabrielSuarez <suarezgabrielr@gmail.com>
2023-02-26 17:04:34 -0400

    css del inicio de sesion

--- src/Componentes/Layout/Layout.jsx
+++ src/Componentes/Layout/Layout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom"
 
 export function Layout() {
   return (
-    <main>
+    <main className="bg-blue-400 max-h-fit">
       <Navbar />
       <section className="body">
         <Outlet />
