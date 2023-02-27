5ae24cf2dd6850e3ceb68cd2ee5911fb3f7bef70
JohnDSmallpage <john.smallpage@correo.unimet.edu.ve>
2023-02-27 18:10:13 -0400

    Arreglado bugs con nombres de clases

--- src/Componentes/Navbar/Navbar.jsx
+++ src/Componentes/Navbar/Navbar.jsx
@@ -6,16 +6,16 @@ import { HOME_URL, LOGIN_URL, REGISTER_URL } from '../../constantes/urls';
 
 export function Navbar() {
   return (
-    <nav id="header" class="w-full z-30 top-10 py-1 shadow-lg border-b border-blue-400 ">
-      <div class="w-full flex items-center justify-between mt-0 px-6 py-2">
-      <div class="group inline-block md:hidden block">
+    <nav id="header" className="w-full z-30 top-10 py-1 shadow-lg border-b border-blue-400 ">
+      <div className="w-full flex items-center justify-between mt-0 px-6 py-2">
+      <div className="group inline-block md:hidden block">
             <button
-                class="outline-none focus:outline-none border px-3 py-1 bg-white rounded-sm flex items-center min-w-32"
+                className="outline-none focus:outline-none border px-3 py-1 bg-white rounded-sm flex items-center min-w-32"
             >
-                <span class="pr-1 font-semibold flex-1">Menu</span>
+                <span className="pr-1 font-semibold flex-1">Menu</span>
                 <span>
                 <svg
-                    class="fill-current h-4 w-4 transform group-hover:-rotate-180
+                    className="fill-current h-4 w-4 transform group-hover:-rotate-180
                     transition duration-150 ease-in-out"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
@@ -27,20 +27,20 @@ export function Navbar() {
                 </span>
             </button>
             <ul
-                class="bg-white border rounded-sm transform scale-0 group-hover:scale-100 absolute 
+                className="bg-white border rounded-sm transform scale-0 group-hover:scale-100 absolute 
             transition duration-150 ease-in-out origin-top min-w-32"
             >
-                <li  class="rounded-sm px-3 py-1 hover:bg-gray-100"><Link to={HOME_URL}>Inicio</Link></li>
-                <li class="rounded-sm px-3 py-1 hover:bg-gray-100"><Link to={HOME_URL}>Nosotros</Link></li>
-                <li class="rounded-sm px-3 py-1 hover:bg-gray-100"><Link to={HOME_URL}>FAQ</Link></li>
+                <li  className="rounded-sm px-3 py-1 hover:bg-gray-100"><Link to={HOME_URL}>Inicio</Link></li>
+                <li className="rounded-sm px-3 py-1 hover:bg-gray-100"><Link to={HOME_URL}>Nosotros</Link></li>
+                <li className="rounded-sm px-3 py-1 hover:bg-gray-100"><Link to={HOME_URL}>FAQ</Link></li>
             </ul>
             </div>
-         <div class="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1 overflow-y-auto" id="menu">
+         <div className="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1 overflow-y-auto" id="menu">
             <nav>
-               <ul class="md:flex items-center justify-between text-base text-white pt-4 md:pt-0">
-                  <li><Link to={HOME_URL} class="inline-block no-underline hover:text-black font-medium text-lg py-2 px-4 lg:-ml-2" >Inicio</Link></li>
-                  <li><Link to={HOME_URL} class="inline-block no-underline hover:text-black font-medium text-lg py-2 px-4 lg:-ml-2" >Nosotros</Link></li>
-                  <li><Link to={HOME_URL} class="inline-block no-underline hover:text-black font-medium text-lg py-2 px-4 lg:-ml-2" >FAQ</Link></li>
+               <ul className="md:flex items-center justify-between text-base text-white pt-4 md:pt-0">
+                  <li><Link to={HOME_URL} className="inline-block no-underline hover:text-black font-medium text-lg py-2 px-4 lg:-ml-2" >Inicio</Link></li>
+                  <li><Link to={HOME_URL} className="inline-block no-underline hover:text-black font-medium text-lg py-2 px-4 lg:-ml-2" >Nosotros</Link></li>
+                  <li><Link to={HOME_URL} className="inline-block no-underline hover:text-black font-medium text-lg py-2 px-4 lg:-ml-2" >FAQ</Link></li>
                </ul>
             </nav>
          </div>
@@ -48,10 +48,10 @@ export function Navbar() {
 
 
          
-         <div class="order-2 md:order-3 flex flex-wrap items-center justify-end mr-0 md:mr-4" id="nav-content">
-            <div class="auth flex items-center w-full md:w-full">
-               <Link to={LOGIN_URL} class="bg-white text-gray-800  p-2 rounded border border-gray-300 mr-4 hover:bg-gray-100 hover:text-gray-700">Iniciar sesion</Link>
-               <Link to={REGISTER_URL} class="bg-blue-600 text-gray-200  p-2 rounded  hover:bg-blue-500 hover:text-gray-100">Registrarse</Link>
+         <div className="order-2 md:order-3 flex flex-wrap items-center justify-end mr-0 md:mr-4" id="nav-content">
+            <div className="auth flex items-center w-full md:w-full">
+               <Link to={LOGIN_URL} className="bg-white text-gray-800  p-2 rounded border border-gray-300 mr-4 hover:bg-gray-100 hover:text-gray-700">Iniciar sesion</Link>
+               <Link to={REGISTER_URL} className="bg-blue-600 text-gray-200  p-2 rounded  hover:bg-blue-500 hover:text-gray-100">Registrarse</Link>
             </div>
          </div>
       </div>
