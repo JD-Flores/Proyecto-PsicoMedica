8a6fd4083e956fb0b3136df214e4ae15edbaac13
GabrielSuarez <suarezgabrielr@gmail.com>
2023-03-14 22:32:48 -0400

    registro doc y validaciones de registro

--- src/Paginas/registerPatient/RegisterPatientPage.jsx
+++ src/Paginas/registerPatient/RegisterPatientPage.jsx
@@ -1,11 +1,17 @@
-import { LOGIN_URL, PERFIL_CLIENTE } from '../../constantes/urls'
+import { LOGIN_URL, PERFIL_CLIENTE, REGISTER_PATIENT_URL } from '../../constantes/urls'
 import { Link, useNavigate } from 'react-router-dom'
-import { signInWithGoogle } from '../../firebase/auth-service'
+import { completed, returnError, signInWithGoogle } from '../../firebase/auth-service'
 import { useState } from 'react'
 import { registerWithEmailAndPassword } from '../../firebase/auth-service'
 
 export function RegisterPatientPage() {
     const navigate = useNavigate();
+    const [error, setError] = useState("");
+    const [errorName, setErrorName] = useState("");
+  const [errorEmail, setErrorEmail] = useState("");
+  const [errorPhone, setErrorPhone] = useState("");
+  const [errorPassword, setErrorPassword] = useState("");
+  const [errorConfirm, setErrorConfirm] = useState("");
     const [formData,setFormData] =useState({
         doctor:false,
         name:"",
@@ -18,14 +24,45 @@ export function RegisterPatientPage() {
 //const para el login con google
     const handleSigninWithGoogle = async ()=>{
         await signInWithGoogle();
-        navigate(PERFIL_CLIENTE)
+        if(completed()){
+            navigate(PERFIL_CLIENTE)
+          }else{
+            navigate(REGISTER_PATIENT_URL)
+          }
+        
     }
 
     const onSubmit = async(event)=>{
         event.preventDefault();//evita que el form recargue la pagina
-        const{email,password,...extraData}=formData//form destructurado
+        if(formData.name==""){
+            setError("Los campos marcados con * en rojo son obligatorios")
+            setErrorName("*")
+          }
+          if(formData.email==""){
+            setError("Los campos marcados con * en rojo son obligatorios")
+            setErrorEmail("*")
+                }
+          if(formData.phone==""){
+            setError("Los campos marcados con * en rojo son obligatorios")
+            setErrorPhone("*")
+          }
+          if(formData.password==""){
+            setError("Los campos marcados con * en rojo son obligatorios")
+            setErrorPassword("*")
+          }
+          if(formData.confirmPassword==""){
+            setError("Los campos marcados con * en rojo son obligatorios")
+            setErrorConfirm("*")
+          }else{
+            const{email,password,...extraData}=formData//form destructurado
         await registerWithEmailAndPassword(email,password,extraData);
-        navigate(PERFIL_CLIENTE)
+        if(completed()){
+            navigate(PERFIL_CLIENTE)
+          }else{
+            setError(returnError())
+          }
+          }
+        
     }
 //en cada input utiliza la info del campo para agregarla al form existente
     const handleOnChange = (event)=>{
@@ -54,34 +91,44 @@ export function RegisterPatientPage() {
         <p className="text-slate-500 ">Correo y contraseña</p>
             <div className="flex flex-col space-y-5">
             <label htmlFor="name">
-                    <p className="font-medium text-slate-700 pb-2">Nombre completo</p>
+            <div className='flex flex-row'>
+                <h1 className="font-medium text-slate-700 pb-2">Nombre completo</h1><p className='text-red-600'>{errorName}</p>
+                </div>
                     <input 
                     id="name" name="name" type="text" 
                     onChange={handleOnChange}
                     className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow" placeholder="Ingresa tu nombre completo"/>
                 </label>
                 <label htmlFor="email">
-                    <p className="font-medium text-slate-700 pb-2">Direccion de correo</p>
+                <div className='flex flex-row'>
+                <h1 className="font-medium text-slate-700 pb-2">Direccion de correo</h1><p className='text-red-600'>{errorEmail}</p>
+                </div>  
                     <input 
                     id="email" name="email" type="email" 
                     onChange={handleOnChange}
                     className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow" placeholder="Ingresa dirección de correo"/>
                 </label>
                 <label htmlFor="telefono">
-                    <p className="font-medium text-slate-700 pb-2">Numero de telefono</p>
+                <div className='flex flex-row'>
+                    <h1 className="font-medium text-slate-700 pb-2">Telefono</h1><p className='text-red-600'>{errorPhone}</p>
+                    </div>
                     <input 
                     id="phone" name="phone" type="text" 
                     onChange={handleOnChange}
                     className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow" placeholder="Ingresa tu número de telefono"/>
                 </label>
                 <label htmlFor="password">
-                    <p className="font-medium text-slate-700 pb-2">Contraseña</p>
+                <div className='flex flex-row'>
+                    <h1 className="font-medium text-slate-700 pb-2">Contraseña</h1><p className='text-red-600'>{errorPassword}</p>
+                    </div>
                     <input id="password" name="password" type="password" 
                     onChange={handleOnChange}
                     className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow" placeholder="Ingresa tu contraseña"/>
                 </label>
                 <label htmlFor="confirmar">
-                    <p className="font-medium text-slate-700 pb-2">Confirmar Contraseña</p>
+                <div className='flex flex-row'>
+                  <h1 className="font-medium text-slate-700 pb-2">Confirmar contraseña</h1><p className='text-red-600'>{errorConfirm}</p>
+                  </div>
                     <input id="confirmPassword" name="confirmPassword" type="password" 
                     onChange={handleOnChange}
                     className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow" placeholder="Ingresa nuevamente la contraseña"/>
@@ -97,6 +144,7 @@ export function RegisterPatientPage() {
                         <a href="#" className="font-medium text-indigo-600">Forgot Password?</a>
                     </div>
                 </div>
+                <p className='text-red-700'>{error}</p> 
                 <button className="w-full py-3 font-medium text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
