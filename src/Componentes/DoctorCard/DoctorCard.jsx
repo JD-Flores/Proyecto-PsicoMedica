a680217d9093945d2e8a8284b3f84d51bd4584aa
JohnDSmallpage <john.smallpage@correo.unimet.edu.ve>
2023-03-18 18:32:08 -0400

    nuevo campo para url de usuarios

--- src/Componentes/DoctorCard/DoctorCard.jsx
+++ src/Componentes/DoctorCard/DoctorCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export function DoctorCard({info}) {
   return (
     <div className='flex flex-col items-center bg-white'>
-        <img className='rounded-full w-[110px] h-[110px] mb-2' src={""} alt="Image from doctor" />
+        <img className='rounded-full w-[110px] h-[110px] mb-2' src={info.profilePic} alt="Image from doctor" />
         <h1 className='mb-2'>Psicólogo</h1>
         <h1 className='text-[13px] text-center mb-2'>{info.name}</h1>
         <h1>Descripción</h1>
