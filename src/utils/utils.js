fb63ec2b2caf4eeab50b0b9b1ca2817c66e38375
JohnDSmallpage <john.smallpage@correo.unimet.edu.ve>
2023-03-21 07:15:37 -0400

    Actualizar foto de perfil para cliente

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,2 @@
-import { ref, uploadBytes,getDownloadURL } from "firebase/storage"
-import { store } from "../firebase/config"
-import { v4 } from "uuid";
 
 
-export const uploadFile = async (file) => {
-    const storageRef = ref(store, `profilePictures/${v4()}`);
-    await uploadBytes (storageRef, file);
-    const url =  await getDownloadURL(storageRef);
-    return url;
-  }
